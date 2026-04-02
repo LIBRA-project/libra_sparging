5585d9c29728e80bb99e308a43dd7958a4b9a43c
hmonroedd <hippolyte.monroe@epfl.ch>
2026-04-02 12:42:54 -0400

    moved correlations to dedicated .py file, added config.py for shared ressources

---
+++ config.py
@@ -0,0 +1,16 @@
+from pint import UnitRegistry
+import scipy.constants as const
+
+VERBOSE = False
+
+ureg = UnitRegistry()
+ureg.formatter.default_format = ".3e~D"
+ureg.define("triton = [tritium] = T")
+ureg.define(f"molT = {const.N_A} * triton")
+ureg.define(f"molT2 = 2 * {const.N_A} * triton")
+ureg.define("neutron = [neutron] = n")
+ureg.define("sccm = 7.44e-7 mol/s")
+
+
+const_R = const.R * ureg("J/K/mol")  # ideal gas constant
+const_g = const.g * ureg("m/s**2")  # gravitational acceleration
