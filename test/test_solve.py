ba31a4ef95a7031f379708707d542a51717e857c
hmonroedd <hippolyte.monroe@epfl.ch>
2026-04-08 16:04:43 -0400

    use @pytest.fixture to avoid modifications propagating to different tests

--- test/test_solve.py
+++ test/test_solve.py
@@ -21,15 +21,18 @@ standard_input = SimulationInput(
     source_T=8e-16 * ureg("molT/m^3/s"),
 )
 
-standard_simulation = Simulation(
-    standard_input,
-    t_final=6 * ureg.hours,
-    signal_irr=lambda t: 1 if t > 1 * ureg.hour and t < 3 * ureg.hour else 0,
-    signal_sparging=lambda t: 1,
-)
+
+@pytest.fixture
+def standard_simulation():
+    return Simulation(
+        standard_input,
+        t_final=6 * ureg.hours,
+        signal_irr=lambda t: 1 if t > 1 * ureg.hour and t < 3 * ureg.hour else 0,
+        signal_sparging=lambda t: 1,
+    )
 
 
-def test_model_solve_successfull(tmp_path):
+def test_model_solve_successfull(tmp_path, standard_simulation):
     """
     Tests that `model.solve` runs without errors for a simple test case. Does not check results.
     Also tests successful exporting results to yaml, json and csv files.
@@ -43,7 +46,7 @@ def test_model_solve_successfull(tmp_path):
     output.profiles_to_csv(Path(tmp_path))
 
 
-def test_model_solve_missing_input():
+def test_model_solve_missing_input(standard_simulation):
     """
     Tests SimulationInput raises error when a required input quantity is missing.
     """
@@ -59,7 +62,7 @@ def test_model_solve_missing_input():
         standard_simulation.solve(dt=0.05 * ureg.hour, dx=0.01 * ureg.m)
 
 
-def test_model_solve_wrong_input():
+def test_model_solve_wrong_input(standard_simulation):
     """
     Tests Simulation.solve() raises error when required input has wrong dimensionality
     """
@@ -74,7 +77,7 @@ def test_model_solve_wrong_input():
         standard_simulation.solve(dt=0.05 * ureg.hour, dx=0.01 * ureg.m)
 
 
-def test_model_solve_wrong_argument():
+def test_model_solve_wrong_argument(standard_simulation):
     """
     Tests Simulation.solve() can't be given a timestep without specifying the units
     """
